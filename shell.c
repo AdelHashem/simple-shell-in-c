e7a7649a24b69d7cd9b16f7290a27b3e57d6f931
AdelHashem <96683697+AdelHashem@users.noreply.github.com>
2022-03-22 17:43:43 +0200

    Add Background_detect fun
    Background_detect to detect & and return 1 if it found and 0 if not

--- shell.c
+++ shell.c
@@ -17,20 +17,32 @@ int main(){
     return 0;
 }
 
-void Fetch(){
+void Fetch(){ // Get The I/P and split it to arguments
     char *temp;
     char buffer[512];
-    memset ( buffer, '\0', 512 );
+    memset ( buffer, '\0', 512 ); // set the values to Null
     fgets(buffer, 512, stdin);
     
-    char *token = strtok(buffer, " ");
+    char *token = strtok(buffer, " "); //split every word when catch space
     int i = 0;
     while(token)
     {
-        token[strcspn(token, "\n")] = 0;
+        token[strcspn(token, "\n")] = 0; // Delete Every /n in the string
         arg[i++] = token;
         temp = token;
         token = strtok(NULL, " ");
     }
     arg[++i]= NULL;
+}
+
+int Background_detect()// detect The & in the command 
+{
+    int i = 0;
+    while(arg[i])
+    {
+        if(arg[i][0] == '&')
+        return 1;
+        i++;
+    }
+    return 0;
 }
