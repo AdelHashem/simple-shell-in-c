4f8657a7397044073deb3468b40b2f973ea8797c
AdelHashem <96683697+AdelHashem@users.noreply.github.com>
2022-03-22 20:10:36 +0200

    Add process fun
    update Background_detect to turn the & to NULL
    Add Process fun Which use Fork

--- shell.c
+++ shell.c
@@ -4,16 +4,21 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
+#include <sys/wait.h>
 
 char *arg[5] = {};
 void Fetch();
+int Background_detect();
+void process();
 
 int main(){
+    
     welcomeScreen();
     while(1)
     {
         Fetch();
-        execvp(arg[0], arg);
+        
+        process();
     }
     return 0;
 }
@@ -41,18 +46,49 @@ int Background_detect()// detect The & in the command
     int i = 0;
     while(arg[i])
     {
-        if(arg[i][0] == '&')
-        return 1;
+        if(arg[i][0] == '&'){
+            arg[i] = NULL;
+            return 1;}
         i++;
     }
     return 0;
+}
+
+void process()
+{
+    pid_t pid = fork();
+    int waitStatus;
+    int flag = 0;
+    flag = Background_detect();
+    if(pid == -1)
+        printf("error occured in fork\n");
+    else if(pid == 0)//child
+    {
+        execvp(arg[0], arg);
+    }
+    else   //Parent (the main process)
+    {
+        
+        if (flag == 1)   //see if there is "&"
+        {
+            return;  //don't wait the child to end
+        }
+        else // wait the child
+        {
+            if (waitpid(pid, &waitStatus, WUNTRACED | WCONTINUED) == -1) {
+                printf("error occured in waitpidn");
+                exit(EXIT_FAILURE);
+    }
+        }
+    }
+
 }
 
 void welcomeScreen(){
         printf("\n\t============================================\n");
         printf("\t               Simple C Shell\n");
         printf("\t--------------------------------------------\n");
-        printf("\t             Adel Hashem (18010880)");
+        printf("\t             Adel Hashem (18010880)\n");
         printf("\t============================================\n");
         printf("\n\n");
 }
