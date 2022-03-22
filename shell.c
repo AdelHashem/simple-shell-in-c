d246bc85d6fee2eeb87cc5722deba17fd8418655
AdelHashem <96683697+AdelHashem@users.noreply.github.com>
2022-03-22 15:08:26 +0200

    Fix Fetch fun & testing execvp
    solve /n problem at the end of token

--- shell.c
+++ shell.c
@@ -5,29 +5,33 @@
 #include <unistd.h>
 #include <errno.h>
 
-char *arg[10] = {} ;
+char *arg[5] = {};
 void Fetch();
 
 int main(){
     //arg = (char*) malloc(512 * sizeof(char));
     Fetch();
-    printf("%s",arg[0]);
-    printf("%s",arg[1]);
-    printf("%s",arg[3]);
-
+    execvp(arg[0], arg);
+   
+    
 
     return 0;
 }
 
 void Fetch(){
+    char *temp;
     char buffer[512];
+    memset ( buffer, '\0', 512 );
     fgets(buffer, 512, stdin);
+    
     char *token = strtok(buffer, " ");
     int i = 0;
     while(token)
     {
+        token[strcspn(token, "\n")] = 0;
         arg[i++] = token;
+        temp = token;
         token = strtok(NULL, " ");
     }
-    arg[++i]= "\0" ;
+    arg[++i]= NULL;
 }
