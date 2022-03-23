67dc72e0929dd4864edcd0dcb42502761d9f7f79
AdelHashem <96683697+AdelHashem@users.noreply.github.com>
2022-03-23 23:51:26 +0200

    Handle CD command with Change_dir fun
    Add Change_dir fun
    fix some compiler warns

--- shell.c
+++ shell.c
@@ -6,45 +6,53 @@
 #include <errno.h>
 #include <sys/wait.h>
 
-char *arg[5] = {};
+
 void Fetch();
 int Background_detect();
 void process();
-
+void Change_Dir(const char *param);
+char *arg[20] = {};
 int main(){
-    
+
     welcomeScreen();
     while(1)
     {
+        memset ( arg, '\0', 20 );
         char cwd[300];
         getcwd(cwd,sizeof(cwd));
         printf("%s>",cwd);
         Fetch();
 
+        if(strcmp(arg[0],"cd") == 0){
+            char para[256];
+            strcpy(para,arg[1]);
+        Change_Dir(para);
+        continue;
+        }else
+        {
         process();
+        }
     }
     return 0;
 }
 
 void Fetch(){ // Get The I/P and split it to arguments
-    char *temp;
     char buffer[512];
     memset ( buffer, '\0', 512 ); // set the values to Null
     fgets(buffer, 512, stdin);
-    
+
     char *token = strtok(buffer, " "); //split every word when catch space
     int i = 0;
     while(token)
     {
         token[strcspn(token, "\n")] = 0; // Delete Every /n in the string
         arg[i++] = token;
-        temp = token;
         token = strtok(NULL, " ");
     }
     arg[++i]= NULL;
 }
 
-int Background_detect()// detect The & in the command 
+int Background_detect()// detect The & in the command
 {
     int i = 0;
     while(arg[i])
@@ -76,7 +84,7 @@ void process()
     }
     else   //Parent (the main process)
     {
-        
+
         if (flag == 1)   //see if there is "&"
         {
             return;  //don't wait the child to end
@@ -88,6 +96,32 @@ void process()
                 exit(EXIT_FAILURE);
     }
         }
+    }
+
+}
+
+void Change_Dir(const char* param){
+    char path[512] = {};
+    if(param[0] == '/')
+    {
+        if(chdir(param) < 0)
+            perror("chdir");
+
+    }else if(param[0] == '~')
+    {
+        char username[50];
+        getlogin_r(username,sizeof(username));
+        strcpy(path,"/home/");
+        strcat(path,username);
+        strcat(path,param+1);
+        if(chdir(path) < 0)
+            perror("chdir");
+    }else{
+        getcwd(path,sizeof(path));
+        strcat(path,"/");
+        strcat(path,param);
+        if(chdir(path) < 0)
+            perror("chdir");
     }
 
 }
